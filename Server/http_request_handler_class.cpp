8e299a84a88ea74065edf3b9f03f41239d6c1dd7
Dilshod Davletyarov <dilshod95@gmail.com>
2024-07-29 16:32:24 +0200

    comment added

--- Server/http_request_handler_class.cpp
+++ Server/http_request_handler_class.cpp
@@ -12,41 +12,7 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 #define MAX_CONNECTIONS 100
-
-//set NON_BLOCKING SOCKET MODE
-// void	set_nonblocking_socket(int socket_fd)
-// {
-// 	int flags = fcntl(socket_fd, F_GETFL, 0);
-// 	if (flags == -1) {
-// 		perror("fcntl F_GETFL");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	if (fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK) == -1) {
-// 		perror("fcntl F_SETFL");
-// 		exit(EXIT_FAILURE);
-// 	}
-// }
-
-// //Set socket options to address binding error and load balance enhancement
-// void	set_options_socket(int socket_fd)
-// {
-// 	int opt = 1;
-// 	if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt))) {
-// 		perror("setsockopt");
-// 		close(socket_fd);
-// 		exit(EXIT_FAILURE);
-// 	}
-// }
-
-// //Bind the socket to the network address and port
-// void	set_bind_name_socket(int socket_fd, sockaddr_in address)
-// {
-// 	if (bind(socket_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-// 		perror("bind failed");
-// 		close(socket_fd);
-// 		exit(EXIT_FAILURE);
-// 	}
-// }
+#define BACKLOG 3
 
 void request_handling(Server server)
 {
@@ -59,8 +25,8 @@ void request_handling(Server server)
 int	main() {
 	Server server;
 
-	server.initEndpoint(8080);
-	server.listenPort(3);
+	server.initEndpoint(PORT);
+	server.listenPort(BACKLOG);
 	std::cout << "Server is listening on port " << PORT << std::endl;
 	request_handling(server);
 	return 0;
