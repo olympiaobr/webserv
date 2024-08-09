1b005807f8876a573948a0fa697f49c4210b6d2a
Dilshod Davletyarov <dilshod95@gmail.com>
2024-08-09 21:09:42 +0200

    print info improved

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -316,7 +316,7 @@ void Server::RUN(std::vector<Server> servers) {
 			servers[i].setResBuffer(buffer, buffer_size);
 		}
 		servers[i].listenPort(BACKLOG);
-		std::cout << BLUE << "Server 1 is listening on port "
+		std::cout << BLUE << "Server " << i + 1 << " is listening on port "
 			<< servers[i].getPort() << RESET << std::endl;
 	}
 	while (true)
